3cee301377169ee4b25d2d3e9fa2a5434f47ad83
Gabriel Garcia <philiasmaximus@gmail.com>
2022-05-30 00:19:23 -0300

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  if (typeof name === 'undefined' || typeof age === 'undefined') {
+    return undefined;
+  }
+  return (`Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`);
+};
+
+// console.log(vqv('Roberto', 40));
 
 module.exports = vqv;
